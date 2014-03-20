770c2ea646cec39e71c09cae5de353cb2deb3deb
ThrowDice <areyoulookon@gmail.com>
2014-03-20 12:07:08 +0800

    Refactor code

--- prime.py
+++ prime.py
@@ -5,7 +5,7 @@ def getPrimeList(n):
     is_prime = 1;
     if n == 2:
         return [2]
-    for i in range(2, int(n)/2+1):
+    for i in range(2, n/2+1):
         if n%i == 0:
             result.extend(getPrimeList(n/i))
             result.extend(getPrimeList(i))
